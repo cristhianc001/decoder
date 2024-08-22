526d7bc6c4b26c0250ac451a14db6f3fa114760e
Cristhian Castro Arias <cristhiancastro001@gmail.com>
2024-08-21 21:16:10 -0500

    js condiciones iniciales

--- app/app.js
+++ app/app.js
@@ -1,1 +1,18 @@
 
+function asignarTextoElemento (elemento, texto){
+    let elementoHTML = document.querySelector(elemento); 
+    elementoHTML.innerHTML = texto;
+    return; 
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento ("h1", "Prueba 2");
+    asignarTextoElemento ("p", "Prueba parrafo");
+    asignarTextoElemento (".message-title", "Prueba parrafo3");
+    asignarTextoElemento (".message-text", "Prueba parrafo4");
+    asignarTextoElemento (".condicion", "Prueba parrafo5");
+    
+}
+
+
+condicionesIniciales();
