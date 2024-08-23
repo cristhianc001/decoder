3a5a3829dfdf10346468f3f6f3ee3a82f6c8f541
Cristhian Castro Arias <cristhiancastro001@gmail.com>
2024-08-22 20:51:54 -0500

    encriptar v1

--- app/app.js
+++ app/app.js
@@ -5,14 +5,36 @@ function asignarTextoElemento (elemento, texto){
     return; 
 }
 
+function asignarImagenElemento(elemento, ruta){
+    let elementoHTML = document.querySelector(elemento); 
+    elementoHTML.src = ruta; 
+    return;
+}
+
 function condicionesIniciales(){
     asignarTextoElemento ("h1", "Prueba 2");
     asignarTextoElemento ("p", "Prueba parrafo");
-    asignarTextoElemento (".message-title", "Prueba parrafo3");
-    asignarTextoElemento (".message-text", "Prueba parrafo4");
-    asignarTextoElemento (".condicion", "Prueba parrafo5");
+    asignarTextoElemento (".contenido__principal__resultado__titulo", "Prueba parrafo3");
+    asignarTextoElemento (".contenido__principal__resultado__info", "Prueba parrafo4");
+    asignarTextoElemento (".contenido__principal__condicion", "Prueba parrafo5");
+    asignarImagenElemento (".contenido__principal__resultado__imagen", "assets/x.png");
     
 }
+
+function encriptar(){
+    let textoUsuario = document.getElementById('textoUsuario').value;
+    asignarTextoElemento (".contenido__principal__resultado__titulo", "");
+    asignarTextoElemento (".contenido__principal__resultado__info", "texto encriptado");
+    
+    let imagen = document.querySelector(".contenido__principal__resultado__imagen"); 
+    imagen.style.width = "1px";   
+    imagen.style.height = "1px";  
+    
+    console.log(textoUsuario);
+    return;
+
+}
+
 
 
 condicionesIniciales();
