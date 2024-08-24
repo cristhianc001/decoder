905c06614121b7f8b1487eccf11b2f6f0e260005
Cristhian Castro Arias <cristhiancastro001@gmail.com>
2024-08-23 21:33:00 -0500

    encriptar y desencriptar

--- app/app.js
+++ app/app.js
@@ -1,4 +1,6 @@
 
+let claveEncriptacion = 1234
+
 function asignarTextoElemento (elemento, texto){
     let elementoHTML = document.querySelector(elemento); 
     elementoHTML.innerHTML = texto;
@@ -12,8 +14,6 @@ function asignarImagenElemento(elemento, ruta){
 }
 
 function condicionesIniciales(){
-    asignarTextoElemento ("h1", "Prueba 2");
-    asignarTextoElemento ("p", "Prueba parrafo");
     asignarTextoElemento (".contenido__principal__resultado__titulo", "Prueba parrafo3");
     asignarTextoElemento (".contenido__principal__resultado__info", "Prueba parrafo4");
     asignarTextoElemento (".contenido__principal__condicion", "Prueba parrafo5");
@@ -22,18 +22,61 @@ function condicionesIniciales(){
 }
 
 function encriptar(){
+     
     let textoUsuario = document.getElementById('textoUsuario').value;
+    let encriptado = btoa(textoUsuario)
+    
+    
+
+
     asignarTextoElemento (".contenido__principal__resultado__titulo", "");
-    asignarTextoElemento (".contenido__principal__resultado__info", "texto encriptado");
+    asignarTextoElemento (".contenido__principal__resultado__info", encriptado);
     
     let imagen = document.querySelector(".contenido__principal__resultado__imagen"); 
     imagen.style.width = "1px";   
     imagen.style.height = "1px";  
-    
-    console.log(textoUsuario);
+
     return;
+    
 
 }
+
+
+function desencriptar(){
+     
+    let textoUsuario = document.getElementById('textoUsuario').value;
+    let desencriptado = atob(textoUsuario)
+    
+    
+
+
+    asignarTextoElemento (".contenido__principal__resultado__titulo", "");
+    asignarTextoElemento (".contenido__principal__resultado__info", desencriptado);
+    
+    let imagen = document.querySelector(".contenido__principal__resultado__imagen"); 
+    imagen.style.width = "1px";   
+    imagen.style.height = "1px";  
+
+    return;
+    
+
+}
+
+
+function copiar() {
+    // Get the text field
+    let textoCopiado = document.getElementById(".contenido__principal__resultado__info");
+  
+    // Select the text field
+    textoCopiado.select();
+    textoCopiado.setSelectionRange(0, 99999); // For mobile devices
+  
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(textoCopiado.value);
+    
+    // Alert the copied text
+    alert("Copied the text: " + textoCopiado.value);
+  }
 
 
 
