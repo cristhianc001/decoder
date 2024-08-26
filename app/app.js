4a035d7205709710e5b9d536f78e7a898a3d7ada
Cristhian Castro Arias <cristhiancastro001@gmail.com>
2024-08-25 21:37:46 -0500

    responsividad v1

--- app/app.js
+++ app/app.js
@@ -1,6 +1,3 @@
-
-let claveEncriptacion = 1234
-
 function asignarTextoElemento (elemento, texto){
     let elementoHTML = document.querySelector(elemento); 
     elementoHTML.innerHTML = texto;
@@ -14,6 +11,7 @@ function asignarImagenElemento(elemento, ruta){
 }
 
 function condicionesIniciales(){
+    asignarTextoElemento (".contenido__principal__resultado__final", "");
     asignarTextoElemento (".contenido__principal__resultado__titulo", "Ningún mensaje fue encontrado");
     asignarTextoElemento (".contenido__principal__resultado__info", "Ingresa el texto que desees encriptar o desencriptar.");
     asignarImagenElemento (".contenido__principal__resultado__imagen", "./assets/decode.png");
@@ -35,7 +33,8 @@ function encriptar(){
         
 
         asignarTextoElemento (".contenido__principal__resultado__titulo", "");
-        asignarTextoElemento (".contenido__principal__resultado__info", encriptado);
+        asignarTextoElemento (".contenido__principal__resultado__info", "");
+        asignarTextoElemento (".contenido__principal__resultado__final", encriptado);
         
         let imagen = document.querySelector(".contenido__principal__resultado__imagen"); 
         imagen.style.width = 0;   
@@ -58,34 +57,58 @@ function desencriptar(){
 
         let desencriptado = atob(textoUsuario)
         
-
         asignarTextoElemento (".contenido__principal__resultado__titulo", "");
-        asignarTextoElemento (".contenido__principal__resultado__info", desencriptado);
+        asignarTextoElemento (".contenido__principal__resultado__info", "");
+        asignarTextoElemento (".contenido__principal__resultado__final", desencriptado);
         
         let imagen = document.querySelector(".contenido__principal__resultado__imagen"); 
         imagen.style.width = 0;   
         imagen.style.height = 0;  
     }
     return;
-    
+}
 
+function procesar(){
+     
+    let textoUsuario = document.getElementById('textoUsuario').value;
+
+    if (textoUsuario == ""){
+        condicionesIniciales();
+    }     
+    else {
+
+        let textoMinusculas = textoUsuario.toLowerCase();
+        let textoSinAcentos = textoMinusculas.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        document.getElementById('textoUsuario').value = textoSinAcentos;
+
+        asignarTextoElemento (".contenido__principal__resultado__final", ""); 
+        asignarTextoElemento (".contenido__principal__resultado__titulo", "El texto ha sido procesado");
+        asignarTextoElemento (".contenido__principal__resultado__info", "Continue con la encriptacion");
+
+    }
+    return;
 }
 
 
 function copiar() {
  
     // se obtiene el valor del elemento con esta clase, un parrafo en este caso
-    let textoCopiado = document.querySelector(".contenido__principal__resultado__info").textContent;
+    let textoCopiado = document.querySelector(".contenido__principal__resultado__final").textContent;
   
-    let temporal = document.createElement("textarea"); // contenedor temporal que no se vera
-    temporal.value = textoCopiado; // se asigna como valor del contenedor el texto copiado
-    document.body.appendChild(temporal); // se agrega al body del documento para que sea seleccionado y copiado mas adelante
+    if (textoCopiado == "") {
+        alert("No hay texto disponible");
+    }
+    else {
+        let temporal = document.createElement("textarea"); // contenedor temporal que no se vera
+        temporal.value = textoCopiado; // se asigna como valor del contenedor el texto copiado
+        document.body.appendChild(temporal); // se agrega al body del documento para que sea seleccionado y copiado mas adelante
 
-    temporal.select(); // selecciona el valor del contenedor temporal
-    navigator.clipboard.writeText(temporal.value); // se copia ese valor al portapapeles
-    document.body.removeChild(temporal); // eliminar el elemento temporal
+        temporal.select(); // selecciona el valor del contenedor temporal
+        navigator.clipboard.writeText(temporal.value); // se copia ese valor al portapapeles
+        document.body.removeChild(temporal); // eliminar el elemento temporal
 
-    alert("Texto copiado: " + textoCopiado);
+        alert("Texto copiado: " + textoCopiado);
+    }
   }
 
 
